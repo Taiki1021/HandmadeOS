875b70ed4ffc66b3483de6c75d76ab2f2df51de3
Taiki Tomita <g.tomita.t@gmail.com>
2017-06-17 14:53:10 +0900

    boot2.asm:FillMemory関数をWordのみにしてシンプル化

--- boot2.asm
+++ boot2.asm
@@ -3,7 +3,6 @@
 
 
 PM_start:
-;jmp $	;デバッグ用
 mov ax,SysDataSelecter ;各セグメントにデータセレクタの値を代入
 mov ds,ax
 mov es,ax
@@ -15,34 +14,25 @@ mov ax,VideoSelecter	;ビデオメモリーの先頭から0x7FF個のax値を書
 mov es,ax
 mov di,0
 mov bx,0x7FF
-mov al,' '
+mov al,'.'
 mov ah,0xE7
-mov cx,1
-call FillMemory
+call FillMemory_Word
 jmp $			;終了
 
-FillMemory: 
+
+
+FillMemory_Word:
 ;Fill given memory area    
 ;
 ;Arguments
 ;
 ;es=Memory selecter
 ;di=Target address(offset)
-;ax=Char to fill with 
-;bx=Count cx=flag(1=Word 0=Byte)
+;ax=Word to fill with 
+;bx=Count
 
 push di
 push bx
-
-cmp cx,1
-jz FillMemory_WriteW  ;if(CX){FillMemory_WriteW}else{FillMemory_WriteB}
-
-FillMemory_WriteB:
-mov [es:di],al		;1バイトずつ書き込む
-inc di
-dec bx
-jnz FillMemory_WriteB
-jmp FillMemory_End
 
 FillMemory_WriteW:
 mov [es:di],ax		;2バイトずつ書き込む
@@ -54,5 +44,6 @@ FillMemory_End:
 pop bx
 pop di
 ret
+
 
 times 512-($-$$) db 0
