81e00c5418edec7ba3b8c7d9e56d14d21c929b24
Taiki Tomita <g.tomita.t@gmail.com>
2017-06-21 21:55:39 +0900

    boot.asm segment.asm:命令の前にインデントを入れた

--- boot.asm
+++ boot.asm
@@ -2,92 +2,92 @@
 [org 0]
 [bits 16]
 
-jmp 0x07C0:start
+	jmp 0x07C0:start
 
 start:
-mov ax,cs ;ds,ssをcsに合わせる
-mov ds,ax
-mov ss,ax
+	mov ax,cs ;ds,ssをcsに合わせる
+	mov ds,ax
+	mov ss,ax
 
-cli
+	cli
 
 Kernel_Load:
-mov ax,0x1000 ;0x1000:0000にAドライブの0番目のシリンダの1番目のセクタをHead=0で読み込む
-mov es,ax
-mov bx,0
-mov ah,2
-mov al,1
-mov ch,0
-mov cl,2
-mov dh,0
-mov dl,0
-int 0x13
-jc Kernel_Load  ;エラーが起きた場合はリトライ
+	mov ax,0x1000 ;0x1000:0000にAドライブの0番目のシリンダの1番目のセクタをHead=0で読み込む
+	mov es,ax
+	mov bx,0
+	mov ah,2
+	mov al,1
+	mov ch,0
+	mov cl,2
+	mov dh,0
+	mov dl,0
+	int 0x13
+	jc Kernel_Load  ;エラーが起きた場合はリトライ
 
-lgdt [gdtr]
+	lgdt [gdtr]
 
-mov eax,cr0		
-or eax,0x00000001	;保護モードに入る
-mov cr0,eax
+	mov eax,cr0		
+	or eax,0x00000001	;保護モードに入る
+	mov cr0,eax
 
-jmp $+2	
-nop
-nop
+	jmp $+2	
+	nop
+	nop
 
-mov ax,SysDataSelecter
-mov ds,ax
-mov es,ax
-mov fs,ax
-mov gs,ax
-mov ss,ax
+	mov ax,SysDataSelecter
+	mov ds,ax
+	mov es,ax
+	mov fs,ax
+	mov gs,ax
+	mov ss,ax
 
-db 0x66
-db 0x67
-jmp DWORD SysCodeSelecter:0000	;読み込んだプログラムに飛ぶ
+	db 0x66
+	db 0x67
+	jmp DWORD SysCodeSelecter:0000	;読み込んだプログラムに飛ぶ
 
 ;========GDT==========================================
 
 
 gdtr:
-dw gdt_end-gdt-1
-dd gdt+0x7C00
+	dw gdt_end-gdt-1
+	dd gdt+0x7C00
 
 gdt: 
-dw 0
-dw 0
-db 0
-db 0
-db 0
-db 0
+	dw 0
+	dw 0
+	db 0
+	db 0
+	db 0
+	db 0
 
 ;SysCodeSelecter
-dw 0xFFFF
-dw 0x0000
-db 0x01
-db 0x9A
-db 0xCF
-db 0x00
+	dw 0xFFFF
+	dw 0x0000
+	db 0x01
+	db 0x9A
+	db 0xCF
+	db 0x00
 
 ;SysDataSelecter
-dw 0xFFFF
-dw 0x0000
-db 0x01
-db 0x92
-db 0xCF
-db 0x00
+	dw 0xFFFF
+	dw 0x0000
+	db 0x01
+	db 0x92
+	db 0xCF
+	db 0x00
 
 ;VideoSelecter
-dw 0xFFFF
-dw 0x8000
-db 0x0B
-db 0x92
-db 0x40
-db 0x00
+	dw 0xFFFF
+	dw 0x8000
+	db 0x0B
+	db 0x92
+	db 0x40
+	db 0x00
 
 gdt_end:
 
 ;=======================================================
 
-times 510-($-$$) db 0
+	times 510-($-$$) db 0
 
-dw 0xAA55
+	dw 0xAA55
--- segment.asm
+++ segment.asm
@@ -5,44 +5,44 @@ global WriteFar
 
 WriteFar:
 ;void WriteFar(unsigned short Selecter,int offset,void* src,int n);
-push eax 
-push es 
-push edi
-push esi
-push ebx 
+	push eax 
+	push es 
+	push edi
+	push esi
+	push ebx 
 
 ;es=書き込み対象のセグメント　edi=書き込み対象のオフセット　esi=書き込むデータへのポインタ　bx=書き込むデータ量
 
-mov ax,[ss:(esp+22)]  ;es=(第一引数)
-mov es,ax  
+	mov ax,[ss:(esp+22)]  ;es=(第一引数)
+	mov es,ax  
 
 
-mov edi,[ss:(esp+26)] ;edi=(第二引数)
+	mov edi,[ss:(esp+26)] ;edi=(第二引数)
 
-mov esi,[ss:(esp+30)] ;esi=(第三引数)
+	mov esi,[ss:(esp+30)] ;esi=(第三引数)
 
-mov ebx,[ss:(esp+34)] ;ebx=(第四引数)
+	mov ebx,[ss:(esp+34)] ;ebx=(第四引数)
 
 WriteFar_Loop:
-cmp ebx,0       ;While(bx!=0)
-jz WriteFar_End 
+	cmp ebx,0       ;While(bx!=0)
+	jz WriteFar_End 
 
-mov al,[ds:esi] ;[es:edi]=[ds:esi]
-mov [es:edi],al
+	mov al,[ds:esi] ;[es:edi]=[ds:esi]
+	mov [es:edi],al
 
-inc edi    ;edi++ esi++ bx--
-inc esi
-dec ebx
+	inc edi    ;edi++ esi++ bx--
+	inc esi
+	dec ebx
 
-jmp WriteFar_Loop 
+	jmp WriteFar_Loop 
 WriteFar_End:  
 
-pop ebx
-pop esi
-pop edi
-pop es
-pop eax
-ret
+	pop ebx
+	pop esi
+	pop edi
+	pop es
+	pop eax
+	ret
 
 
 Halt:
