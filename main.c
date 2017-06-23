9248eb92855812c1190e638d2b10d420b4a6dbde
Taiki Tomita <g.tomita.t@gmail.com>
2017-06-24 02:26:26 +0900

    ストリング操作及びフォーマット出力のユーティリティを実装

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 
 int main(){
 	int A=0;
+	char Buf[64];
 	vputs("Hello World!!\n");
 	vputs("This is a pen.\n");
-	vputs("This is it\nYes we can!!\n          I can do it!!");
-	while(0){
-		vputc(A%10+'0');
-		vputc('\n');
-		A++;
+	vputs("This is it\nYes we can!!\n          I can do it!!\n");
+	for(A=0;;A++){
+		sformat(Buf,"Your Number is %d\n",A);
+		vputs(Buf);
 	}
 	Halt();
 }
--- video.c
+++ video.c
@@ -1,6 +1,7 @@
 #include"segment.h"
 #include"selecter.h"
 #include"video.h"
+#include<stdarg.h>
 
 unsigned short Counter;
 
@@ -14,10 +15,10 @@ void vputc(char c){
 		CopyFar(VideoSelecter,(char*)(Counter*2),1,SysDataSelecter,&c,1,1);
 		Counter++;
 	}
-	outb(0x3D4,0x0F);
-	outb(0x3D5,Counter);
-	outb(0x3D4,0x0E);
-	outb(0x3D5,Counter >> 8);
+//	outb(0x3D4,0x0F);
+//	outb(0x3D5,Counter);
+//	outb(0x3D4,0x0E);
+//	outb(0x3D5,Counter >> 8);
 }
 
 void vputs(char* str){
@@ -42,3 +43,87 @@ void schroll(){
 	Counter-=80;
 }
 
+
+
+
+
+
+void blockcpy(void* dist,void* src,int size){
+	int A;
+	for(A=0;A<size;A++){
+		((char*)dist)[A]=((char*)src)[A];
+	}
+}
+
+void blockswap(void* A,void* B,int blocksize){
+	char C[32];
+	blockcpy(C,A,blocksize);
+	blockcpy(A,B,blocksize);
+	blockcpy(B,C,blocksize);
+}
+
+void reverse(void* seq,int blocksize,int count){
+	int A;
+	for(A=0;A<=count/2-1;A++){
+		blockswap(seq+blocksize*A,seq+blocksize*(count-A-1),blocksize);
+	}
+}
+
+int strlen(char* str){
+	int A;
+	for(A=0;*str++;A++);
+	return A;
+}
+
+void numstr(char* out,int num){
+	char* head=out;
+	if(num==0){
+		*head='0';
+		head++;
+	}
+	while(num>0){
+		*head=num%10+'0';
+		num/=10;
+		head++;
+	}
+	*head=0;
+	reverse(out,sizeof(char),strlen(out));
+}
+
+void sformat(char* dist,char* form,...){
+	int A;
+	char* head=dist;
+	char* sub;
+	va_list ap;
+
+	va_start(ap,form);
+
+	for(A=0;form[A];A++){
+		if(form[A]=='%'){
+			switch(form[A+1]){
+			case 'd':
+				numstr(head,va_arg(ap,int));
+				head+=strlen(head);
+				break;
+			case 's':
+				sub=va_arg(ap,char*);
+				blockcpy(head,sub,sizeof(char)*strlen(sub));
+				head+=strlen(sub);
+				break;
+			case 'c':
+				*head=va_arg(ap,int);
+				head++;
+			case '%':
+				*head='%';
+				head++;
+				break;
+			}
+			A+=1;
+		}else{
+			*head=form[A];
+			head++;
+		}
+	}
+	*head=0;
+	va_end(ap);
+}
